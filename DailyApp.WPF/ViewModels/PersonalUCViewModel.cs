c20757acf19415ad62e0d3ae0c0dcef3e75bf8c7
Administrator <Administrator@SK-20220405SVPP>
2024-05-03 23:15:59 +0800

    #个性化功能实现

--- DailyApp.WPF/ViewModels/PersonalUCViewModel.cs
+++ DailyApp.WPF/ViewModels/PersonalUCViewModel.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using MaterialDesignColors;
+using MaterialDesignColors.ColorManipulation;
+using MaterialDesignThemes.Wpf;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace DailyApp.WPF.ViewModels
 {
-    internal class PersonalUCViewModel
+    internal class PersonalUCViewModel : BindableBase
     {
+        public IEnumerable<ISwatch> Swatches { get; } = SwatchHelper.Swatches;
+        public DelegateCommand<Object> ChangeHueCommand { get; private set; }
+        private readonly PaletteHelper paletteHelper = new();
+
+        private bool _isDarkTheme;
+        public bool IsDarkTheme
+        {
+            get => _isDarkTheme;
+            set
+            {
+                if (SetProperty(ref _isDarkTheme, value))
+                {
+                    ModifyTheme(theme => theme.SetBaseTheme(value ? BaseTheme.Light : BaseTheme.Dark));
+                }
+            }
+        }
+
+        public PersonalUCViewModel()
+        {
+            ChangeHueCommand = new DelegateCommand<Object>(ChangeHue);
+        }
+        private static void ModifyTheme(Action<Theme> modificationAction)
+        {
+            var paletteHelper = new PaletteHelper();
+            Theme theme = paletteHelper.GetTheme();
+
+            modificationAction?.Invoke(theme);
+
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void ChangeHue(object? obj)
+        {
+            var hue = (Color)obj!;
+
+            Theme theme = paletteHelper.GetTheme();
+
+            theme.PrimaryLight = new ColorPair(hue.Lighten());
+            theme.PrimaryMid = new ColorPair(hue);
+            theme.PrimaryDark = new ColorPair(hue.Darken());
+
+            paletteHelper.SetTheme(theme);
+
+        }
     }
 }
